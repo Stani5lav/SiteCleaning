c77f7ddf35f389ed65bbdbace0191b024667cdf6
Stani5lav <stasbai@i.ua>
2023-10-02 16:01:27 +0300

    sliderReady

--- script.js
+++ script.js
@@ -28,6 +28,29 @@ btnCloseMenu.onclick = function (e) {
     sidebarMenu.classList.remove('opened');
 }
 
+/**
+ * 
+ * Slider
+ * 
+ */
+
+let sliderNav = document.querySelectorAll('.slider-nav li');
+
+for (let i = 0; i < sliderNav.length; i++) {
+    sliderNav[i].onclick = function (e) {
+        e.preventDefault();
+        document.querySelector('.slider-nav li.active').classList.remove('active');
+        e.target.classList.add('active');
+
+        let id = e.target.dataset.id;
+
+        document.querySelector('.slider-item.active').classList.remove('active');
+        document.querySelector('.slider-item[data-id="' + id + '"]').classList.add('active');
+
+
+    }
+}
+
 
 
 
