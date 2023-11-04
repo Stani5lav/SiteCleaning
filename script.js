72e4e6d740b4c831f9a562842c8c58cca0680fee
Stani5lav <stasbai@i.ua>
2023-11-04 20:21:52 +0200

    gulp added

--- script.js
+++ script.js
@@ -34,23 +34,40 @@ btnCloseMenu.onclick = function (e) {
  * 
  */
 
-let sliderNav = document.querySelectorAll('.slider-nav li');
+// let sliderNav = document.querySelectorAll('.slider-nav li');
 
-for (let i = 0; i < sliderNav.length; i++) {
-    sliderNav[i].onclick = function (e) {
-        e.preventDefault();
-        document.querySelector('.slider-nav li.active').classList.remove('active');
-        e.target.classList.add('active');
+// for (let i = 0; i < sliderNav.length; i++) {
+//     sliderNav[i].onclick = function (e) {
+//         e.preventDefault();
+//         document.querySelector('.slider-nav li.active').classList.remove('active');
+//         e.target.classList.add('active');
 
-        let id = e.target.dataset.id;
+//         let id = e.target.dataset.id;
 
-        document.querySelector('.slider-item.active').classList.remove('active');
-        document.querySelector('.slider-item[data-id="' + id + '"]').classList.add('active');
+//         document.querySelector('.slider-item.active').classList.remove('active');
+//         document.querySelector('.slider-item[data-id="' + id + '"]').classList.add('active');
 
 
-    }
-}
+//     }
+// }
 
+
+$('.slider-nav li').on('click', function (e) { //(для об'єкту лі) (підключаємо подію (он)) "клік", яка виконує функцію
+    e.preventDefault();
+
+    $('.slider-nav li.active').removeClass('active'); //видалити клас ектів у об'екта лі
+    let id = $(this).data('id'); //створити змінну айді, яка зберігає у собі атрибут айді поточного лі та (строка 68)
+
+    $('.slider-item.active').fadeOut("slow", function () { //плавно приховати активний об'єкт слайдер-айтем та виконати функцію 
+        $(this).removeClass('active'); //видалити клас ектів у поточного активного об'єкту слайдер-айтем
+        $('.slider-item[data-id="' + id + '"]').fadeIn("slow", function () { //плавно показати об'єкт слайдер-айтем із обраним айді та виконати функцію
+            $(this).addClass('active'); //додати йому клас ектів
+        });
+
+    });
+    $(this).addClass('active'); //(зі строки 59) та додати йому клас ектів
+
+});
 
 
 
